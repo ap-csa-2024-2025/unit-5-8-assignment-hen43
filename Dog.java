84cdf59cf8e305a0a1df47b9b54767b200619a21
Henry Le <140691753+hen43@users.noreply.github.com>
2025-01-08 19:05:16 +0000

    gyatt

--- Dog.java
+++ Dog.java
@@ -4,13 +4,23 @@ public class Dog
   private int ID;
   private static int numDogs = 0;
 
-  // Non-default Dog constructor
 
+  // Non-default Dog constructor
+  public Dog(string name){
+    this.name = name;
+    numDogs++;
+    this.ID = numDogs * numDogs;
+  }
   // Default Dog constructor
 
   // Setters - instance variables only
+  private void setName(String name){this.name = name;}
+  private void setID(int ID){this.ID = ID;}
+  private void setNum(int numDogs){this.numDogs = numDogs;}
 
   // Getters - instance variables only
-
+  private String getName(){return name;}
+  private int getID(){return ID;}
+  private int getNumDogs(){return numDogs;}
   // method getNumDogs
 }
